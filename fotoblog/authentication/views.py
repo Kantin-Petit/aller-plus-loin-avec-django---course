7b649bfc52e76671bad3912d3231c9bbed9ddb8a
Kantin-Petit <kantin.dev@gmail.com>
2023-12-28 12:42:17 +0100

    chap 2

--- fotoblog/authentication/views.py
+++ fotoblog/authentication/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from telnetlib import LOGOUT
+from django.contrib.auth import authenticate, login, logout
+from . import forms
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                message = 'Nom d\'utilisateur ou mot de passe incorrect.'
+    return render(request, 
+                  'authentication\login.html', 
+                  {'form': form, 'message': message})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
